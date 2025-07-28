36728e0eea991e079cf6cb39ff52b9ac705ee1c7
Wink Saville <wink@saville.com>
2025-07-27 17:38:35 -0700

    feat: Get running using csgrs v0.20.1
    ATM it only creates a cube
    Cargo.toml assumes ../csgrs/ is csgrs and that v0.20.1 is current.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::{Parser, value_parser};
-use csgrs::{csg::CSG, polygon::Polygon};
+//use csgrs::{csg::CSG, polygon::Polygon};
+use csgrs;
+
+type Mesh = csgrs::mesh::Mesh<()>;
 
 // Define the command line arguments
 #[derive(Parser, Debug)]
@@ -44,48 +47,48 @@ struct Args {
 
 }
 
-fn create_text(text: &str, font_data: &[u8], len_side: f64) -> CSG<()> {
-    let csg_text: CSG<()> = CSG::text(text, font_data, 4.5, None);
-    let csg_text_bb = csg_text.bounding_box();
-    //println!("cgs_text_bb: {:?}", csg_text_bb);
-    let csg_text_extents = csg_text_bb.extents();
-    //println!("cgs_text_extents: {:?}", csg_text_extents);
+//fn create_text(text: &str, font_data: &[u8], len_side: f64) -> CSG<()> {
+//    let csg_text: CSG<()> = CSG::text(text, font_data, 4.5, None);
+//    let csg_text_bb = csg_text.bounding_box();
+//    //println!("cgs_text_bb: {:?}", csg_text_bb);
+//    let csg_text_extents = csg_text_bb.extents();
+//    //println!("cgs_text_extents: {:?}", csg_text_extents);
+//
+//    let text_extrude = 0.1;
+//    let text_3d = csg_text.extrude(text_extrude);
+//
+//    // Rotate the text to be on the xz plane
+//    let text_3d = text_3d.rotate(90.0, 0.0, 0.0);
+//
+//    // Position the text in the center of face on xz plane
+//    // and sink 10% of the extrude depth into the cube to
+//    // be sure there are no holes in the print caused by
+//    // the text not being exactly on the surface.
+//    let half_len_side = len_side / 2.0;
+//    let half_extents_y = csg_text_extents.y / 2.0;
+//    let half_extents_x = csg_text_extents.x / 2.0;
+//    let text_sink_depth = text_extrude * 0.10;
+//    text_3d.translate(
+//        half_len_side - half_extents_x,
+//        -text_sink_depth,
+//        half_len_side - half_extents_y,
+//    )
+//}
 
-    let text_extrude = 0.1;
-    let text_3d = csg_text.extrude(text_extrude);
-
-    // Rotate the text to be on the xz plane
-    let text_3d = text_3d.rotate(90.0, 0.0, 0.0);
-
-    // Position the text in the center of face on xz plane
-    // and sink 10% of the extrude depth into the cube to
-    // be sure there are no holes in the print caused by
-    // the text not being exactly on the surface.
-    let half_len_side = len_side / 2.0;
-    let half_extents_y = csg_text_extents.y / 2.0;
-    let half_extents_x = csg_text_extents.x / 2.0;
-    let text_sink_depth = text_extrude * 0.10;
-    text_3d.translate(
-        half_len_side - half_extents_x,
-        -text_sink_depth,
-        half_len_side - half_extents_y,
-    )
-}
-
-fn print_polygons(polygons: &[Polygon<()>]) {
-    //println!("polygon: {:?}", polygon);
-    for (idx, polygon) in (polygons.iter()).enumerate() {
-        println!("polygon {idx}:");
-        println!(" vertices count: {}", polygon.vertices.len());
-        //println!(" vertices: {:?}", polygon.vertices);
-        for (idx, vertex) in (polygon.vertices.iter()).enumerate() {
-            println!("  vertex {idx}:");
-            println!("     pos: {:?}", vertex.pos);
-            println!("     normal: {:?}", vertex.normal);
-        }
-        println!(" plane: {:?}", polygon.plane());
-    }
-}
+//fn print_polygons(polygons: &[Polygon<()>]) {
+//    //println!("polygon: {:?}", polygon);
+//    for (idx, polygon) in (polygons.iter()).enumerate() {
+//        println!("polygon {idx}:");
+//        println!(" vertices count: {}", polygon.vertices.len());
+//        //println!(" vertices: {:?}", polygon.vertices);
+//        for (idx, vertex) in (polygon.vertices.iter()).enumerate() {
+//            println!("  vertex {idx}:");
+//            println!("     pos: {:?}", vertex.pos);
+//            println!("     normal: {:?}", vertex.normal);
+//        }
+//        //println!(" plane: {:?}", polygon.plane());
+//    }
+//}
 
 /// Create a cube with an optional tube in the center.
 /// The tube is created by removing the material defined by the tube from the cube.
@@ -95,13 +98,14 @@ fn print_polygons(polygons: &[Polygon<()>]) {
 /// * `len_side` - The length of the sides of the cube
 /// * `tube_diameter` - The diameter of the tube to create in the center of the cube, 0.0 for no tube
 /// * `segments` - The number of segments to use when creating the tube, minimum is 3
-fn create_cube(len_side: f64, tube_diameter: f64, segments: u32, print_polygons_flag: bool) -> CSG<()> {
+fn create_cube(len_side: f64, _tube_diameter: f64, segments: u32, _print_polygons_flag: bool) -> Mesh {
     if segments < 3 {
         panic!("segments must be 3 or greater");
     }
 
     // Create the cube
-    let mut cube = CSG::cube(len_side, len_side, len_side, None);
+    //let mut cube = Mesh::cube(len_side, None); //len_side, len_side, None);
+    let cube = Mesh::cube(len_side, None); //len_side, len_side, None);
     //let geometry = &cube.geometry;
     //println!("geometry: {:?}", geometry);
     //let polygons = &cube.to_polygons();
@@ -109,28 +113,28 @@ fn create_cube(len_side: f64, tube_diameter: f64, segments: u32, print_polygons_
     //let polygons = &cube.polygons;
     //println!("polygons: {:?}", polygons);
 
-    if  print_polygons_flag {
-        // Print the polygons of the cube
-        println!("cube polygons:");
-        print_polygons(&cube.polygons);
-    }
+    //if  print_polygons_flag {
+    //    // Print the polygons of the cube
+    //    println!("cube polygons:");
+    //    print_polygons(&cube.polygons);
+    //}
 
     // Create the tube and translate it to the center of the cube
-    if tube_diameter > 0.0 {
-        // Create the tube and remove the material it's from the cube
-        let tube_radius = tube_diameter / 2.0;
-        let tube = CSG::cylinder(tube_radius, len_side, segments as usize, None);
-        let tube = tube.translate(len_side / 2.0, len_side / 2.0, 0.0);
-        cube = cube.difference(&tube);
+    //if tube_diameter > 0.0 {
+    //    // Create the tube and remove the material it's from the cube
+    //    let tube_radius = tube_diameter / 2.0;
+    //    let tube = CSG::cylinder(tube_radius, len_side, segments as usize, None);
+    //    let tube = tube.translate(len_side / 2.0, len_side / 2.0, 0.0);
+    //    cube = cube.difference(&tube);
 
-        // Create the text for the tube diameter
-        let font_data = include_bytes!("../fonts/courier-prime-sans/courier-prime-sans.ttf");
-        let text = format!("{:3}", (tube_diameter * 1000.0) as usize);
-        let text_3d = create_text(&text, font_data, len_side);
+    //    // Create the text for the tube diameter
+    //    //let font_data = include_bytes!("../fonts/courier-prime-sans/courier-prime-sans.ttf");
+    //    //let text = format!("{:3}", (tube_diameter * 1000.0) as usize);
+    //    //let text_3d = create_text(&text, font_data, len_side);
 
-        // Union the cube with the text
-        cube = cube.union(&text_3d);
-    }
+    //    // Union the cube with the text
+    //    cube = cube.union(&text_3d);
+    //}
 
     // Return the finished cube
     cube
